334ce7c14299907e1165e6791c9e7daf0932e57c
Andrew Guggenberger <aguggenberger@users.noreply.github.com>
2021-09-02 08:57:02 -0500

    Update simulation.ps1

--- simulation.ps1
+++ simulation.ps1
@@ -1,5 +1,4 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
 
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/aguggenberger/iv-lab/main/simulation.tmp' -OutFile 'simulation.exe'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/aguggenberger/iv-lab/main/simulation.tmp' -OutFile '$env:temp\simulation.exe'
 
-ls
